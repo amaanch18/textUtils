5ac18cc6f987916790a3c74a991a9dd51bca7286
amaanch18 <amaanch18@gmail.com>
2022-09-28 02:12:04 +0530

    Updates

---
+++ precache-manifest.e02b6b34453f2908b8591b38a2fa4184.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ad7cf1707873b696504f",
+    "url": "/textUtils/static/css/main.d6f94a58.chunk.css"
+  },
+  {
+    "revision": "ad7cf1707873b696504f",
+    "url": "/textUtils/static/js/main.ad7cf170.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/textUtils/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "946d8365c0317c9ba6aa",
+    "url": "/textUtils/static/js/2.946d8365.chunk.js"
+  },
+  {
+    "revision": "9a7857878919569515e3",
+    "url": "/textUtils/static/js/runtime~main.9a785787.js"
+  },
+  {
+    "revision": "d28f094ba60f42af37c8fbd854b14ce7",
+    "url": "/textUtils/index.html"
+  }
+];
