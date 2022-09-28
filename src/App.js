04ce337e40b375458180fcb98e88c593354d2755
amaanch18 <amaanch18@gmail.com>
2022-09-28 22:24:11 +0530

    Textutils Done

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 
 import './App.css';
-// import About from './components/About';
+import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import React,{useState} from 'react'
 import Alert from './components/Alert';
-// import {
-//   BrowserRouter as Router,
-//   Routes,
-//   Route,
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
 
-// } from "react-router-dom";
+} from "react-router-dom";
 
 
 function App() {
-  const [mode, setMode] = useState('light');// whether dark mode is enabled or not
+  const [mode, setMode] = useState('light');
   const [alert, setAlert] = useState(null);
 
   const showAlert = (message , type) => {
@@ -32,44 +32,26 @@ function App() {
       setMode('dark');
       document.body.style.backgroundColor = 'rgb(3 20 62)';
       showAlert("Dark mode has been enabled","success");
-      document.title = 'TextUtils - Dark Mode';
     }
     else{
       setMode('light');
       document.body.style.backgroundColor = 'white'
       showAlert("Light mode has been enabled","success");
-      document.title = 'TextUtils - Light Mode';
     }
   }
 
-  // const greenMode =() => {
-  //   if( mode === 'light'){
-  //     setMode('green');
-  //     document.body.style.backgroundColor = '#003615';
-  //     showAlert("Green mode has been enabled","success");
-  //   }
-  //   else{
-  //     setMode('light');
-  //     document.body.style.backgroundColor = 'white'
-  //     showAlert("Light mode has been enabled","success");
-  //   }
-  // }
-  
   return (
     <>  
-    {/* <Navbar title="Textutils" aboutText="About Text" /> */}
-    
-    {/* <Router> */}
+    <Router>
     <Navbar title="Textutils" mode={mode} toggleMode={toggleMode} />
-    <Alert alert = {alert} />
+    <Alert alert = {alert}  />
     <div className="container my-3" >
-      {/* <Routes> */}
-        {/* <Route exact path="/" element ={<TextForm showAlert= {showAlert} heading="Enter the text to analyze below" mode = {mode} />}/> */}
-        <TextForm showAlert= {showAlert} heading="Enter the text to analyze below" mode = {mode} />
-        {/* <Route exact path="/about" element = {<About />}/> */}
-      {/* </Routes> */}
+      <Routes>
+        <Route exact path="/" element ={<TextForm showAlert= {showAlert} heading="Enter the text to analyze below" mode = {mode} />}/>
+        <Route exact path="/about" element = {<About mode={mode} toggleMode = {toggleMode}/>}/>
+      </Routes>
     </div>
-    {/* </Router> */}
+    </Router>
     </>
   );
 }
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -29,9 +29,7 @@ export default function TextForm(props) {
     }
 
     const handleCopy = () => {
-        var text = document.getElementById("myBox");
-        text.select();
-        navigator.clipboard.writeText(text.value)
+        navigator.clipboard.writeText(text);
         props.showAlert("Text Copied", "success");
     }
 
@@ -39,22 +37,22 @@ export default function TextForm(props) {
     return (
         <>
         <div className="container" style={{color: props.mode === 'dark'?'white':'black'}}>
-            <h1>{props.heading} </h1>
+            <h1 className='mb-4'>{props.heading} </h1>
             <div className="mb-3">
                 <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor: props.mode === 'dark'?'grey':'white' , color: props.mode === 'dark'?'white':'black' }} id="myBox" rows="8"></textarea>
             </div>
-            <button className="btn btn-primary mx-1" onClick={handleUpClick} >Convert to upper case</button>
-            <button className="btn btn-primary mx-1" onClick={handleLoClick} >Convert to lower case</button>
-            <button className="btn btn-primary mx-1" onClick={handleClearClick} >Clear Text</button>
-            <button className="btn btn-primary mx-1" onClick={handleReverseClick} >Reverse Text</button>
-            <button className="btn btn-primary mx-1" onClick={handleCopy} >Copy Text</button>
+            <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleUpClick} >Convert to upper case</button>
+            <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleLoClick} >Convert to lower case</button>
+            <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleClearClick} >Clear Text</button>
+            <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleReverseClick} >Reverse Text</button>
+            <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleCopy} >Copy Text</button>
         </div>
         <div className="container my-3" style={{color: props.mode === 'dark'?'white':'black'}}>
             <h2>Your Text Summary</h2>
-            <p>{text.split(" ").length} words and {text.length} characters</p>
-            <p>It will take {0.008 * text.split(" ").length} minutes to read</p>
+            <p>{text.split(/\s+/).filter((element)=>{return element.length!==0}).length} words and {text.length} characters</p>
+            <p>It will take {0.008 * text.split(" ").filter((element)=>{return element.length!==0}).length} minutes to read</p>
             <h2>Preview</h2>
-            <p>{text.length>0?text:"Enter something in the textbox above to Preview it here"}</p>
+            <p>{text.length>0?text:"Nothing to preview!"}</p>
         </div>
         </>
     )
